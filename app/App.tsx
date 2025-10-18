fe5edcf49bbba8c026e373847538237551809224
jonoya <jonoyanguren@gmail.com>
2025-10-18 22:53:17 +0200

    Convert app submodule to regular directory

---
+++ app/App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Smart Cashless Hub</Text>
+      <Text style={styles.subtitle}>Marketing, Analytics & Loyalty Platform</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  subtitle: {
+    marginTop: 10,
+    fontSize: 14,
+    color: '#666',
+  },
+});
